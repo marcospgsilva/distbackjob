7ff37c404bcdb1d97d66ddf954217f7396a3f342
Marcos Paulo <marcos@meucredere.com.br>
2021-12-10 22:17:44 -0300

    add libcluster

---
+++ lib/dist_back_job/application.ex
@@ -0,0 +1,29 @@
+defmodule DistBackJob.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      {Cluster.Supervisor, [topologies(), [name: DistBackJob.ClusterSupervisor]]}
+      # Starts a worker by calling: DistBackJob.Worker.start_link(arg)
+      # {DistBackJob.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: DistBackJob.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp topologies do
+    [
+      background_job: [
+        strategy: Cluster.Strategy.Gossip
+      ]
+    ]
+  end
+end
